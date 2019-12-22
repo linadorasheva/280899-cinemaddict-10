7848f0126698ecbe0ccf7aaef94abdc1bc3f5ea6
lina_dorasheva <dorasheva.a@gmail.com>
2019-12-22 22:22:07 +0300

    Добавит функции генерации даты и описания фильма

--- src/components/card-template.js
+++ src/components/card-template.js
@@ -5,17 +5,25 @@ const getButtonClass = (flag) => {
 export const createCardTemplate = (card) => {
   const {filmPosterSrc, filmName, filmDescription, filmRating, filmDate, filmDuration, filmGenre, filmComments, isAddWatchList, isWatched, isFavorite} = card;
 
+  const getShortFilmDescription = () => {
+    return filmDescription.length > 139 ? `${filmDescription.slice(0, 139)}...` : filmDescription;
+  };
+
+  const getShortDate = () => {
+    return filmDate.getFullYear();
+  };
+
   return (
     `<article class="film-card">
       <h3 class="film-card__title">${filmName}</h3>
       <p class="film-card__rating">${filmRating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">${filmDate}</span>
+        <span class="film-card__year">${getShortDate()}</span>
         <span class="film-card__duration">${filmDuration}</span>
         <span class="film-card__genre">${filmGenre}</span>
       </p>
       <img src="${filmPosterSrc}" alt="" class="film-card__poster">
-      <p class="film-card__description">${filmDescription}</p>
+      <p class="film-card__description">${getShortFilmDescription()}</p>
       <a class="film-card__comments">${filmComments}</a>
       <form class="film-card__controls">
         <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${getButtonClass(isAddWatchList)}">Add to watchlist</button>
