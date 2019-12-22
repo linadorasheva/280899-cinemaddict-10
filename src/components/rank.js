35405482f4e29b8f6eef1e59b8d29e244806b5ed
lina_dorasheva <dorasheva.a@gmail.com>
2019-12-22 15:51:12 +0300

    Добавит разметку звания пользователя

--- src/components/rank.js
+++ src/components/rank.js
@@ -1,7 +1,11 @@
-export const createRank = () => {
+import {getRank} from '../mock/rank.js';
+
+export const createRank = (quantity) => {
+  const rankName = getRank(quantity);
+
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${rankName}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
