0b3bc577de1577a2cf7f0868fd58df536a0bca5b
angelina.dorasheva <dorasheva.a@gmail.com>
2019-12-23 19:44:05 +0300

    Добавит мок-данные расширенной инф о фильме

--- src/mock/card.js
+++ src/mock/card.js
@@ -1,6 +1,23 @@
 import * as util from '../util.js';
 
-const filmNames = [`Побег из Шоушенка`, `Форрест Гамп`, `Крестный отец`, `Интерстеллар`, `Властелин колец`, `Нокдаун`, `Прислуга`, `В погоне за счастьем`, `Адвокат дьявола`, `Воин`, `Гаттака`, `Бойцовский клуб`, `Терминал`, `Титаник`];
+const filmNames = [`Побег из Шоушенка`, `Форрест Гамп`, `Крестный отец`, `Интерстеллар`, `Нокдаун`, `Прислуга`, `В погоне за счастьем`, `Адвокат дьявола`, `Воин`, `Гаттака`, `Бойцовский клуб`, `Терминал`, `Титаник`, `Семь`];
+
+const filmOriginalsName = {
+  'Побег из Шоушенка': `The Shawshank Redemption`,
+  'Форрест Гамп': `Forrest Gump`,
+  'Крестный отец': `The Godfather`,
+  'Интерстеллар': `Interstellar`,
+  'Нокдаун': `Cinderella Man`,
+  'Прислуга': `The Help`,
+  'В погоне за счастьем': `The Pursuit of Happyness`,
+  'Адвокат дьявола': `The Devil's Advocate`,
+  'Воин': `Warrior`,
+  'Гаттака': `Gattaca`,
+  'Бойцовский клуб': `Fight Club`,
+  'Терминал': `The Terminal`,
+  'Титаник': `Titanic`,
+  'Семь': `Se7en`
+};
 
 const filmDescriptionTemplate = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
@@ -61,10 +78,17 @@ const getDate = () => {
   return date;
 };
 
+const filmDirector = [`James Cameron`, `David Fincher`, `Christopher Nolan`, `Gavin O'Connor`, `Francis Ford Coppola`, `Tate Taylor`];
+const filmWriter = [`James Cameron`, `Chuck Palahniuk`, `Christopher Nolan`, `Gavin O'Connor`, `Cliff Dorfman`, `Anthony Tambakis`, `Francis Ford Coppola`, `Tate Taylor`];
+const filmActors = [`Robert De Niro`, `Gwyneth Paltrow`, `Al Pacino`, `Brad Pitt`, `Leonardo DiCaprio`, `Tom Cruise`, `Kate Winslet`, `Tom Hardy`, `Joel Edgerton`, `Marlon Brando`, `Emma Stone`];
+const filmCountries = [`USA`, `UK`, `Italy`, `Spain`, `France`, `Japan`, `Norway`, `Portugal`, `Mexico`];
+const filmAgeRatings = [0, 6, 12, 16, 18];
+
 const generateCard = () => {
   return {
     filmPosterSrc: generateFilmPosterSrc(),
     filmName: util.getRandomArrayItem(filmNames),
+    filmOriginalName: filmOriginalsName,
     filmDescription: generateFilmDescription(filmDescriptionTemplate),
     filmRating: generateFilmRating(),
     filmDate: getDate(),
@@ -74,6 +98,11 @@ const generateCard = () => {
     isAddWatchList: Math.random() > 0.5,
     isWatched: Math.random() > 0.5,
     isFavorite: Math.random() > 0.5,
+    filmDirector: util.getRandomArrayItem(filmDirector),
+    filmWriters: new Set(filmWriter),
+    filmActors: new Set(filmActors),
+    filmCountry: util.getRandomArrayItem(filmCountries),
+    filmAgeRating: util.getRandomArrayItem(filmAgeRatings)
   };
 };
 
