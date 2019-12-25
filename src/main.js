2e2c2b348c97073c33c9b0466a49b4c273bc1770
angelina.dorasheva <dorasheva.a@gmail.com>
2019-12-25 17:47:10 +0300

    Добавит функцию отрисовки по 5 карточек

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {shuffleArray, isEscPress} from './util.js';
+import {isEscPress} from './util.js';
 
 import {createMenu} from './components/menu.js';
 import {createRank} from './components/rank.js';
@@ -10,17 +10,20 @@ import {createPopup} from './components/popup.js';
 import {generateCards} from './mock/card.js';
 
 
-const QUANTITY_CARDS = 100;
+const QUANTITY_CARDS = 15;
+const SHOWING_CARDS_COUNT_ON_START = 5;
+const SHOWING_CARDS_COUNT_BY_BUTTON = 5;
+
 const cards = generateCards(QUANTITY_CARDS);
 
-const getMarkupCards = (count, data) => {
-  const markupCards = shuffleArray(data)
-    .slice(0, count)
-    .map((it) => (createCardTemplate(it)))
-    .join(`\n`);
+// const getMarkupCards = (count, data) => {
+//   const markupCards = shuffleArray(data)
+//     .slice(0, count)
+//     .map((it) => (createCardTemplate(it)))
+//     .join(`\n`);
 
-  return markupCards;
-};
+//   return markupCards;
+// };
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -40,13 +43,26 @@ render(main, createFilmsContainer());
 
 const filmListMain = main.querySelector(`.films-list`);
 render(filmListMain, createShowMoreButton());
+const loadMoreButton = filmListMain.querySelector(`.films-list__show-more`);
 
 const cardBoxMain = main.querySelector(`.films-list .films-list__container`);
 const cardBoxExtra = main.querySelectorAll(`.films-list--extra .films-list__container`);
 const cardBoxTopRating = cardBoxExtra[0];
 const cardBoxMostCommented = cardBoxExtra[1];
 
-render(cardBoxMain, getMarkupCards(5, cards));
+let showingCardsCount = SHOWING_CARDS_COUNT_ON_START;
+cards.slice(0, showingCardsCount).forEach((card) => render(cardBoxMain, createCardTemplate(card)));
+
+loadMoreButton.addEventListener(`click`, () => {
+  const prevCardsCount = showingCardsCount;
+  showingCardsCount = showingCardsCount + SHOWING_CARDS_COUNT_BY_BUTTON;
+
+  cards.slice(prevCardsCount, showingCardsCount).forEach((card) => render(cardBoxMain, createCardTemplate(card)));
+
+  if (showingCardsCount >= cards.length) {
+    loadMoreButton.remove();
+  }
+});
 
 const getTopRatingCardList = () => {
   const topRatingCards = cards.slice().sort((a, b) => b.filmRating - a.filmRating).slice(0, 2);
@@ -58,10 +74,8 @@ const getTopCommentsCardList = () => {
   return topCommentsCards;
 };
 
-
-render(cardBoxTopRating, getMarkupCards(2, getTopRatingCardList()));
-render(cardBoxMostCommented, getMarkupCards(2, getTopCommentsCardList()));
-
+getTopRatingCardList().forEach((card) => render(cardBoxTopRating, createCardTemplate(card)));
+getTopCommentsCardList().forEach((card) => render(cardBoxMostCommented, createCardTemplate(card)));
 
 const onEscPress = () => {
   if (isEscPress) {
