68e4fe75bc4252d3de184e9cdb08e8844ebe04bc
lina_dorasheva <dorasheva.a@gmail.com>
2020-01-20 23:31:26 +0300

    Удалит неиспользуемые данные

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,6 @@
-const MONTH_NAMES = [
-  `January`,
-  `February`,
-  `March`,
-  `April`,
-  `May`,
-  `June`,
-  `July`,
-  `August`,
-  `September`,
-  `October`,
-  `November`,
-  `December`,
-];
-
 const KeyCode = {
   ESC_KEY_CODE: 27,
   ENTER_KEY_CODE: 13
 };
 
-export {MONTH_NAMES, KeyCode};
+export {KeyCode};
